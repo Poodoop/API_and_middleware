6ff7da0a1c2969149f598b5fdf12c46ef7ef26d4
Poodoop <dickyaristyo43@gmail.com>
2023-10-22 01:20:14 +0700

    chore: change route files

--- routes/movies.js
+++ routes/movies.js
@@ -1,87 +1,14 @@
-var express = require('express')
-var path = require('path')
-var multer = require('multer')
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+const Controller = require('../controllers/controller.js');
+const { signToken, verifyToken } = require('../middleware/authMiddleware');
+const diskStorage = require('../middleware/fileMiddleware');
 
-var router = express.Router()
+router.get('/', Controller.getMovies);
+router.post('/', verifyToken, Controller.createMovie);
+router.delete('/:id', verifyToken, Controller.deleteMovie);
+router.put('/:id', verifyToken, Controller.updateMovie);
+router.put('/upload/:id', multer({ storage: diskStorage }).single('photo'), Controller.uploadImage);
 
-var pool = require('../config/queries.js')
-
-const { signToken, verifyToken } = require('../middleware/authMiddleware')
-const diskStorage = require('../middleware/fileMiddleware')
-
-router.get('/', function (req, res) {
-    const limit = parseInt(req.query.limit) || 10
-    const page = parseInt(req.query.page) || 1
-
-    const startIndex = (page - 1) * limit
-
-    pool.query(
-        'SELECT * FROM movies OFFSET $1 LIMIT $2',
-        [startIndex, limit], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.json(results.rows)
-        }
-    )
-})
-
-router.post('/', verifyToken, function (req, res) {
-    const { title, genres, year } = req.body
-    pool.query(
-        'INSERT INTO movies ("title", "genres", "year") VALUES ($1, $2, $3)',
-        [title, genres, year], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'success' })
-        }
-    )
-})
-
-router.delete('/:id', verifyToken, function (req, res) {
-    const movieId = req.params.id
-    pool.query(
-        'DELETE FROM movies WHERE id = $1',
-        [movieId], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'success' })
-        }
-    )
-})
-
-router.put('/:id', verifyToken, function (req, res) {
-    const movieId = req.params.id
-    const { year } = req.body
-    pool.query(
-        'UPDATE movies SET year = $1 WHERE id = $2',
-        [year, movieId], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'success' })
-        }
-    )
-})
-
-router.put('/upload/:id', multer({ storage: diskStorage }).single('photo'), function (req, res) {
-    const file = req.file.path
-    const movieId = req.params.id
-
-    if (!file) {
-        res.status(400).send({ status: false, data: 'No File is selected.', })
-    }
-    pool.query(
-        'UPDATE movies SET images = $1 WHERE id = $2',
-        [file, movieId], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'success' })
-        }
-    )
-})
-
-module.exports = router
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,81 +1,12 @@
-var express = require('express')
-var router = express.Router()
-
-var pool = require('../config/queries.js')
-
+var express = require('express');
+var router = express.Router();
+const Controller = require('../controllers/controller.js');
 const { signToken, verifyToken } = require('../middleware/authMiddleware');
 
-router.post('/login', function (req, res) {
-    const { email, password } = req.body
-    pool.query(
-        `SELECT * FROM users WHERE email = $1 AND password = $2`,
-        [email, password], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            } else {
-                const token = signToken(results.rows[0])
-                res.json({ token: token })
-            }
-        }
-    )
-})
+router.post('/login', Controller.login);
+router.post('/register', Controller.register);
+router.get('/', Controller.getUsers);
+router.delete('/:id', verifyToken, Controller.deleteUser);
+router.put('/:id', verifyToken, Controller.updateUser);
 
-router.post('/register', function (req, res) {
-    const { id, email, gender, role, password } = req.body
-    pool.query(
-        'INSERT INTO users (id, email, gender, role, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [id, email, gender, role, password], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'User registered successfully' })
-        }
-    )
-})
-
-router.get('/', function (req, res) {
-    const limit = parseInt(req.query.limit) || 10
-    const page = parseInt(req.query.page) || 1
-
-    const startIndex = (page - 1) * limit
-
-    pool.query(
-        'SELECT id, email, gender, role, password FROM users OFFSET $1 LIMIT $2',
-        [startIndex, limit], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.json(results.rows)
-        }
-    )
-})
-
-router.delete('/:id', verifyToken, function (req, res) {
-    const userId = req.params.id
-    pool.query(
-        'DELETE FROM users WHERE id = $1',
-        [userId], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'success' })
-        }
-    )
-})
-
-
-router.put('/:id', verifyToken, function (req, res) {
-    const userId = req.params.id
-    const { password } = req.body
-    pool.query(
-        'UPDATE users SET password = $1 WHERE id = $2',
-        [password, userId], (error, results) => {
-            if (error) {
-                return res.status(500).json({ error: 'Internal server error' })
-            }
-            res.status(201).json({ status: 'success' })
-        }
-    )
-})
-
-module.exports = router
+module.exports = router;
